1ab676e8dcedb88d01ae1242bee66cc1379fe978
mohamedsaeid1998 <105775732+mohamedsaeid1998@users.noreply.github.com>
2023-12-29 14:57:42 +0200

    fix bugs

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -12,23 +12,10 @@ import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from 'recharts'
 
 const Home = () => {
 
+  const { headers, userRole, adminData } = useContext(AuthContext)
+  console.log(adminData);
 
-
-
-
-
-
-
-
-
-
-
-
-
-  const { headers,userRole,adminData } = useContext(AuthContext)
-console.log(adminData);
-
-console.log(userRole);
+  console.log(userRole);
 
 
   const { data: tableData } = UseAuthenticatedQuery({
@@ -58,60 +45,139 @@ console.log(userRole);
         console.log(err);
       })
   }
-console.log();
+  console.log();
 
   useEffect(() => {
-if (userRole==="Manager"){
-  console.log("here")
-  getUsersCount()
-}
+
+    // if (userRole === "Manager") {
+      getUsersCount()
+    // } else {
+    //   return null
+    // }
+
 
 
   }, [])
 
 
-  const cardsDetails = [
+  const activated = userCount?.activatedEmployeeCount;
+  const deactivated = userCount?.deactivatedEmployeeCount;
+
+  const TasksDetails = [
     { cardColor: "tasksTodo", iconBgColor: "tasksBg", icon: TodoIcon, status: "Todo", number: toDo },
     { cardColor: "tasksProgress", iconBgColor: "progressBg", icon: ProgressIcon, status: "InProgress", number: inProgress },
     { cardColor: "tasksDone", iconBgColor: "doneBg", icon: DoneIcon, status: "Done", number: done }
   ]
 
+  const UsersDetails = [
+    { cardColor: "tasksTodo", iconBgColor: "tasksBg", icon: TodoIcon, status: "ActiveUser", number: activated },
+    { cardColor: "tasksProgress", iconBgColor: "progressBg", icon: ProgressIcon, status: "DesActiveUser", number: deactivated },
+  ]
 
-const activated = userCount?.activatedEmployeeCount;
-const deactivated = userCount?.deactivatedEmployeeCount;
 
-  const data = [
+
+  const TasksData = [
     { name: "ToDo", value: toDo, color: "#0088FE" },
     { name: "InProgress", value: inProgress, color: "#00C49F" },
     { name: "Done", value: done, color: "#FFBB28" },
+  ];
+
+
+  const UsersData = [
     { name: "ActiveEmp", value: activated, color: "#FF8042" },
     { name: "DeActiveEmp", value: deactivated, color: "#8dd1e1" },
   ];
 
-  const data2 = [
-    { name: "ToDo", value: toDo, color: "#0088FE" },
-    { name: "InProgress", value: inProgress, color: "#00C49F" },
-    { name: "Done", value: done, color: "#FFBB28" },
-  ];
+
+
+
 
   return <>
     <main className='HomePicture ps-5 py-5 '>
       <div className="container">
 
         <div className='w-100 homeHeader py-5 px-4 text-white mb-4'>
-          <div></div>
+
           <h1 className='mb-4 mt-3 fw-normal '>Welcome <span className='orange'>{adminData?.userName}</span></h1>
           <p className='fs-3 mb-5'>You can add project and assign tasks to your team</p>
         </div>
 
-        <div className="row ">
+        <div className="row g-3 p-2  ">
 
-          <div className="col-md-6 p-3 bg-white homeTasks">
+          <div className="col-md-6 p-3  bg-white homeTasks">
             <h5 className='mb-0'>Tasks</h5>
             <p className='text-muted'>Lorem ipsum dolor sit amet,consecteture</p>
             <div className="row">
               {tableData ? <>
-                {cardsDetails.map((ele) =>
+                {TasksDetails.map((ele) =>
+                  <div key={ele.icon} className="col-md-4 ">
+
+                    <div className={`d-flex flex-column ${ele.cardColor} p-3 rounded-4`}>
+
+                      <div className={`IconContainer ${ele.iconBgColor} p-1 mb-2`}>
+                        <img src={ele.icon} alt="Icon" />
+                      </div>
+                      <h6 className='mb-0'>{ele.status}</h6>
+                      <span className='fs-4 fw-medium'>{ele.number}</span>
+                    </div>
+                  </div>
+                )}</>
+
+                : <LoadingIcon />}
+
+
+
+
+            </div>
+          </div>
+
+
+          <div className="col-md-6 p-3 bg-white ">
+
+            <h5 className='mb-0'>Progress</h5>
+            <p className='text-muted'>Lorem ipsum dolor sit amet,consecteture</p>
+            {tableData ? <><div className="chart">
+              <ResponsiveContainer width="99%" height={150}>
+                <PieChart >
+                  <Tooltip contentStyle={{ background: "white", borderRadius: "5px" }} />
+                  <Pie
+                    data={TasksData}
+                    innerRadius={"70%"}
+                    outerRadius={"90%"}
+                    paddingAngle={5}
+                    dataKey="value"
+                  >
+                    {TasksData.map((item) => (
+                      <Cell key={item.name} fill={item.color} />
+                    ))}
+                  </Pie>
+                </PieChart>
+              </ResponsiveContainer>
+            </div>
+              <div className="options">
+                {TasksData.map((item) => <div key={item.name} className="option">
+                  <div className="titles">
+                    <div className="dot" style={{ backgroundColor: item.color }} />
+                    <span>{item.name}</span>
+                  </div>
+                </div>
+
+                )}
+              </div></> : <LoadingIcon />}
+
+
+          </div>
+
+
+        </div>
+
+        <div className="row g-3 p-2">
+          <div className="col-md-6 p-3 bg-white homeTasks">
+            <h5 className='mb-0'>Users</h5>
+            <p className='text-muted'>Lorem ipsum dolor sit amet,consecteture</p>
+            <div className="row">
+              {tableData ? <>
+                {UsersDetails.map((ele) =>
                   <div key={ele.icon} className="col-md-4 ">
 
                     <div className={`d-flex flex-column ${ele.cardColor} p-3 rounded-4`}>
@@ -135,20 +201,20 @@ const deactivated = userCount?.deactivatedEmployeeCount;
 
           <div className="col-md-6 p-3 bg-white ">
 
-            <h5 className='mb-0'>Progress</h5>
+            <h5 className='mb-0'>Users Statue</h5>
             <p className='text-muted'>Lorem ipsum dolor sit amet,consecteture</p>
-            {userRole==="Manager" ? <>{tableData  ? <><div className="chart">
+            {userCount ? <><div className="chart">
               <ResponsiveContainer width="99%" height={150}>
                 <PieChart >
                   <Tooltip contentStyle={{ background: "white", borderRadius: "5px" }} />
                   <Pie
-                    data={data}
+                    data={UsersData}
                     innerRadius={"70%"}
                     outerRadius={"90%"}
                     paddingAngle={5}
                     dataKey="value"
                   >
-                    {data.map((item) => (
+                    {UsersData.map((item) => (
                       <Cell key={item.name} fill={item.color} />
                     ))}
                   </Pie>
@@ -156,36 +222,7 @@ const deactivated = userCount?.deactivatedEmployeeCount;
               </ResponsiveContainer>
             </div>
               <div className="options">
-                {data.map((item) => <div key={item.name} className="option">
-                  <div className="titles">
-                    <div className="dot" style={{ backgroundColor: item.color }} />
-                    <span>{item.name}</span>
-                  </div>
-                </div>
-
-                )}
-              </div></> : <LoadingIcon />}</> : <>
-              
-              {tableData  ? <><div className="chart">
-              <ResponsiveContainer width="99%" height={150}>
-                <PieChart >
-                  <Tooltip contentStyle={{ background: "white", borderRadius: "5px" }} />
-                  <Pie
-                    data={data2}
-                    innerRadius={"70%"}
-                    outerRadius={"90%"}
-                    paddingAngle={5}
-                    dataKey="value"
-                  >
-                    {data2.map((item) => (
-                      <Cell key={item.name} fill={item.color} />
-                    ))}
-                  </Pie>
-                </PieChart>
-              </ResponsiveContainer>
-            </div>
-              <div className="options">
-                {data2.map((item) => <div key={item.name} className="option">
+                {UsersData.map((item) => <div key={item.name} className="option">
                   <div className="titles">
                     <div className="dot" style={{ backgroundColor: item.color }} />
                     <span>{item.name}</span>
@@ -194,12 +231,13 @@ const deactivated = userCount?.deactivatedEmployeeCount;
 
                 )}
               </div></> : <LoadingIcon />}
-              </>
-              }
-            
+
 
           </div>
         </div>
+
+
+
       </div>
     </main>
   </>
